c9e2354c4a44b5006fbf691f35dd9b78a582f83a
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2018-05-28 17:10:26 +0500

    starter scrip

---
+++ initial.sh
@@ -0,0 +1,9 @@
+wget https://raw.githubusercontent.com/dewsoftpvtltd/emailserver/master/three.sh
+wget https://raw.githubusercontent.com/dewsoftpvtltd/emailserver/master/one.sh
+wget https://raw.githubusercontent.com/dewsoftpvtltd/emailserver/master/two.sh
+
+chmod +x three.sh
+chmod +x one.sh
+chmod +x two.sh
+
+./one.sh
