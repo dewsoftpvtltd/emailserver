919f40facd7d885c7e00170978aade56ea72406c
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2018-05-28 17:28:22 +0500

    starter script

--- one.sh
+++ one.sh
@@ -3,8 +3,8 @@
 sudo apt-get update -y && sudo apt-get dist-upgrade -y
 sudo hostnamectl set-hostname mail.zeeraksoft.com
 cp /etc/hosts /etc/hosts.bak
-grep '^;cgi.fix_pathinfo=1' /etc/hosts
-sed -i 's,^;cgi.fix_pathinfo=1$,cgi.fix_pathinfo=0,' /etc/hosts
+grep '^;127.0.0.1       localhost' /etc/hosts
+sed -i 's,^127.0.0.1       localhost$,127.0.0.1       mail.zeeraksoft.com localhost,' /etc/hosts
 
 
 wget https://bitbucket.org/zhb/iredmail/downloads/iRedMail-0.9.7.tar.bz2
